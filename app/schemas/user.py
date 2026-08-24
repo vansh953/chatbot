418ad1d3f56804da5d49bb7d5e4f9a9050745160
vansh953 <vansh24164035@akgec.ac.in>
2026-08-24 23:40:16 +0530

    remove avatar_url field, not in use

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -25,7 +25,6 @@ class UserProfileUpdate(BaseModel):
     has_hypertension: Optional[bool] = None
     other_conditions: Optional[str] = None
     allergies: Optional[str] = None
-    avatar_url: Optional[str] = None
 
 
 class UserOut(BaseModel):
@@ -40,7 +39,6 @@ class UserOut(BaseModel):
     has_hypertension: bool
     other_conditions: Optional[str] = None
     allergies: Optional[str] = None
-    avatar_url: Optional[str] = None
     created_at: datetime
 
     class Config:
